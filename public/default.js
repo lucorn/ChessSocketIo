8b5f4740ff0f11b8cd9f30d3bbd0e89173383cf3
Cornel Lupu <cornellupu@hotmail.com>
2016-02-21 13:07:30 -0800

    Chess app from sample on channel9
    Chess web app using socket.io

---
+++ public/default.js
@@ -0,0 +1,38 @@
+var board;
+var game;
+
+window.onload = function () {
+    initGame();
+};
+
+var initGame = function() {
+   var cfg = {
+       draggable: true,
+       position: 'start',
+       onDrop: handleMove,
+   };
+
+   board = new ChessBoard('gameBoard', cfg);
+   game = new Chess();
+};
+
+// called when a player makes a move on the board UI
+var handleMove = function(source, target) {
+    var move = game.move({from: source, to: target});
+
+    if (move === null) {
+        return 'snapback';
+    } else {
+        socket.emit('move', move);
+    }
+};
+
+// setup my socket client
+var socket = io();
+
+// called when the server calls socket.broadcast('move')
+socket.on('move', function (msg) {
+    game.move(msg);
+    board.position(game.fen()); // fen is the board layout
+});
+
